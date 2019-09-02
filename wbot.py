8f4a822857dac55e58eb3e3c6cde46ef6d1e5f29
Alvis <falvis@gmail.com>
2019-09-02 16:43:20 +0530

    finally is working

--- wbot.py
+++ wbot.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-from utils import fetch_reply
+from re import fetch_reply
 
 app = Flask(__name__)
 
