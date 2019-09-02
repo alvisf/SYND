88f7576ba2d4240a44c22859196d10166b320d7a
Alvis <falvis@gmail.com>
2019-09-02 15:26:46 +0530

    eror3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
+from utils import fetch_reply
+
 app = Flask(__name__)
 
 
@@ -14,10 +16,12 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
+    phone_no = request.form.get('From')
+    reply = fetch_reply(msg, phone_no)
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    resp.message(reply)
 
     return str(resp)
 
